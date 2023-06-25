572697439364f6525b17dfa3f83cd1a73f03fdec
Mcayear <mcayear@163.com>
2023-06-25 21:25:52 +0800

    更新

--- libs/@LLSELib/error/IllegalArgumentError.js
+++
@@ -1,9 +0,0 @@
-export class IllegalArgumentError extends Error {
-    constructor(message) {
-        super(message);
-        this.name = 'IllegalArgumentError';
-        if (Error.captureStackTrace) {
-            Error.captureStackTrace(this, IllegalArgumentError);
-        }
-    }
-}
